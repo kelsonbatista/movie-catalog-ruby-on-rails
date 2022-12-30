773f4419044b8b4c91ec7729ef564fe6ba80a0d5
kelsonbatista <kelsonbatista@gmail.com>
2022-12-30 10:44:04 -0300

    chore: database relationships

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,2 +1,3 @@
 class Director < ApplicationRecord
+  has_many :movies, class_name: "movie", foreign_key: "director_id"
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,3 @@
 class Genre < ApplicationRecord
+  has_many :movies, class_name: "movie", foreign_key: "genre_id"
 end
