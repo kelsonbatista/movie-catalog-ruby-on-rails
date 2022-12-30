548dcfd9a9af5e7a7c13c52b2678ea079b1ff7fd
kelsonbatista <kelsonbatista@gmail.com>
2022-12-30 16:29:08 -0300

    chore: remove link warnings from chrome

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # erros de links do resources no chrome
+  config.action_view.preload_links_header = false
 end
